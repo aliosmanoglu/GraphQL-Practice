e6d51cd4bc17ef09df5c3957212aa263956986ce
aliosmanoglu <aliguveli0@gmail.com>
2024-08-29 18:35:34 +0300

    commit

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const {ApolloServer, gql} = require("apollo-server");
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+const { createSchema, createYoga } = require('graphql-yoga');
+const { createServer } = require('node:http');
+const { PubSub } = require('graphql-subscriptions');
+const pubsub = new PubSub(); 
 
 
 const data = require('./data');
 
-const typeDefs = gql`
+// const typeDefs = `
 
     
+// `;
 
+
+const yoga = createYoga({
+    schema: createSchema({
+      typeDefs: /* GraphQL */ `
     type User {
         id : Int!,
         username : String!,
@@ -40,45 +49,141 @@ const typeDefs = gql`
         location(id : Int) : [Location],
         participant(id :Int) : [Participant] 
     }
-`;
-
-const resolvers = {
-    Query : {
-        user : (parent,args) => args.id ? [data.users.find((user) => user.id === args.id)] : data.users, //{
-        location : (parent,args) => args.id ? [data.locations.find((location) => location.id === args.id)] : data.locations,
-        participant : (parent,args) => args.id ? [data.participants.find((participant) => participant.id === args.id)] : data.participants,
-        event : (parent,args) => args.id ? [data.events.find((event) => event.id === args.id)] : data.events
-    },
-    User : {
-        events : (parent,args) => {
-            if(args.id) {
-                let filtered = data.events.filter((event) => event.user_id === parent.id);
-                return filtered.filter((event) => event.id === args.id);
-            }else {
-                return data.events.filter((event) => event.user_id === parent.id)
-            }
-        }
-    },
-    Event : {
-        participants : (parent) => data.participants.filter((participants) => participants.event_id === parent.id),
-        user : (parent) =>  data.users.find((user) => parent.user_id === user.id),
-        location : (parent) => data.locations.find((location) => parent.location_id === location.id)
-    },
-    Participant :{
-        username : (parent) => data.users.find((user) => user.id === parent.user_id ).username
+    
+    type Mutation {
+        createUser(id : Int, username : String!, email : String!) : User!
     }
 
-};
+    type Subscription {
+        userCreated : User
+    }
+      `,
+      resolvers:  {
+        Subscription : {
+            userCreated: {
+                subscribe : (_,__, { pubsub }) => pubsub.asyncIterator('userCreated'),
+                resolve: (payload, args) => {
+                    if(payload.userCreated && payload.userCreated.username === "ali") {
+                        console.log(payload.userCreated);
+                        
+                        return payload.userCreated;
+                    }
+                    
+                    return null;
+                  }
+            }
+        },
 
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    introspection: true,  // introspection özelliğini etkinleştir
-    plugins: [
-      ApolloServerPluginLandingPageGraphQLPlayground()  // GraphQL Playground'u etkinleştir
-    ],     
-});
+        Query : {
+            user : (parent,args) => args.id ? [data.users.find((user) => user.id === args.id)] : data.users, //{
+            location : (parent,args) => args.id ? [data.locations.find((location) => location.id === args.id)] : data.locations,
+            participant : (parent,args) => args.id ? [data.participants.find((participant) => participant.id === args.id)] : data.participants,
+            event : (parent,args) => args.id ? [data.events.find((event) => event.id === args.id)] : data.events
+        },
+        User : {
+            events : (parent,args) => {
+                if(args.id) {
+                    let filtered = data.events.filter((event) => event.user_id === parent.id);
+                    return filtered.filter((event) => event.id === args.id);
+                }else {
+                    return data.events.filter((event) => event.user_id === parent.id)
+                }
+            }
+        },
+        Event : {
+            participants : (parent) => data.participants.filter((participants) => participants.event_id === parent.id),
+            user : (parent) =>  data.users.find((user) => parent.user_id === user.id),
+            location : (parent) => data.locations.find((location) => parent.location_id === location.id)
+        },
+        Participant :{
+            username : (parent) => data.users.find((user) => user.id === parent.user_id ).username
+        },
+        Mutation : {
+            createUser : (parent,args) => {
+                const user = {
+                    id : args.id,
+                    username : args.username,
+                    email : args.email
+                };
+    
+                data.users.push(user);
+                pubsub.publish('userCreated', {userCreated : user});
 
 
-server.listen(3000).then(() => console.log("SERVER CALISTI"))
+                return user;
+            }
+        }
+    
+    
+    
+    }
+    }),
+    context: {
+        pubsub, // context içinde pubsub'ı sağlıyoruz
+    },
+  })
+  
+
+// const resolvers = {
+//     Query : {
+//         user : (parent,args) => args.id ? [data.users.find((user) => user.id === args.id)] : data.users, //{
+//         location : (parent,args) => args.id ? [data.locations.find((location) => location.id === args.id)] : data.locations,
+//         participant : (parent,args) => args.id ? [data.participants.find((participant) => participant.id === args.id)] : data.participants,
+//         event : (parent,args) => args.id ? [data.events.find((event) => event.id === args.id)] : data.events
+//     },
+//     User : {
+//         events : (parent,args) => {
+//             if(args.id) {
+//                 let filtered = data.events.filter((event) => event.user_id === parent.id);
+//                 return filtered.filter((event) => event.id === args.id);
+//             }else {
+//                 return data.events.filter((event) => event.user_id === parent.id)
+//             }
+//         }
+//     },
+//     Event : {
+//         participants : (parent) => data.participants.filter((participants) => participants.event_id === parent.id),
+//         user : (parent) =>  data.users.find((user) => parent.user_id === user.id),
+//         location : (parent) => data.locations.find((location) => parent.location_id === location.id)
+//     },
+//     Participant :{
+//         username : (parent) => data.users.find((user) => user.id === parent.user_id ).username
+//     },
+//     Mutation : {
+//         createUser : (parent,args) => {
+//             const user = {
+//                 id : args.id,
+//                 username : args.username,
+//                 email : args.email
+//             };
+
+//             data.users.push(user);
+
+//             return user;
+//         }
+//     }
+
+
+
+// };
+
+
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     introspection: true,  // introspection özelliğini etkinleştir
+//     plugins: [
+//       ApolloServerPluginLandingPageGraphQLPlayground()  // GraphQL Playground'u etkinleştir
+//     ],     
+// });
+
+
+
+const server = createServer(yoga);
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
+
+// server.listen(3000).then(() => console.log("SERVER CALISTI"))
